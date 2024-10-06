d4b719da7c4d28b0898928c37bd0dff1f591ea50
pavelpotapenko <PotapenkoPA@loodsen.ru>
2024-10-06 22:21:14 +0300

    task1 implementation

---
+++ task1/main.c
@@ -0,0 +1,16 @@
+/**
+ * main.c -- программа "Hello, students!"
+ *
+ * Copyright (c) 2022, Pavel Potapenko <potapenko.dev@gmail.com>
+ *
+ * This code is licensed under MIT license.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    fprintf(stdout, "Hello, students!\n");
+
+    return 0;
+}
